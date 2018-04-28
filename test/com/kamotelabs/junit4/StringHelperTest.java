7825c8e574e08bd7f9b567d9c23a3086bf14a261
Marlon <martianworm17@gmail.com>
2018-04-28 14:53:03 +0800

    Refactoring : Improving the JUnit

--- test/com/kamotelabs/junit4/StringHelperTest.java
+++ test/com/kamotelabs/junit4/StringHelperTest.java
@@ -7,16 +7,16 @@ import org.junit.Test;
 import com.kamotelabs.helper.StringHelper;
 
 public class StringHelperTest {
+	
+	StringHelper helper = new StringHelper();
 
 	@Test
 	public void testTruncateAInFirstTwoPositions_AAinFirst2Positions() {
-		StringHelper helper = new StringHelper();
 		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
 	}
 	
 	@Test
 	public void testTruncateAInFirstTwoPositions2_AinFirstPosition() {
-		StringHelper helper = new StringHelper();
 		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
 	}
 
